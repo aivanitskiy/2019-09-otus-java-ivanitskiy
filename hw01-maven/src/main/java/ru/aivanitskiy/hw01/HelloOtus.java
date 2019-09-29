2d0b51f5cc293b624b9e054ad65eba473c571041
aivanitskiy <aivanitskiy@yandex.ru>
2019-09-29 22:08:14 +0300

    using guava

--- hw01-maven/src/main/java/ru/aivanitskiy/hw01/HelloOtus.java
+++ hw01-maven/src/main/java/ru/aivanitskiy/hw01/HelloOtus.java
@@ -1,6 +1,15 @@
 package ru.aivanitskiy.hw01;
 
+import com.google.common.collect.HashMultiset;
+
 public class HelloOtus {
     public static void main(String[] args) {
+        HashMultiset<String> storage = HashMultiset.create();
+        storage.add("one");
+        storage.add("one");
+        storage.add("one");
+
+        System.out.println(storage.count("one"));
+        System.out.println(storage.count("two"));
     }
 }
